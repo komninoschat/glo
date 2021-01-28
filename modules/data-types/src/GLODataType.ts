5948cf9af2754b892165e1b4e22ee008f6fe4951
komninoschat <komninoschat@gmail.com>
2021-01-28 17:18:38 +0200

    provide default implementation for data type operations

--- modules/data-types/src/GLODataType.ts
+++ modules/data-types/src/GLODataType.ts
@@ -1,3 +1,5 @@
+import GLOError from '@glossa-glo/error';
+
 interface GLODataType {
   add(right: GLODataType): GLODataType;
   subtract(right: GLODataType): GLODataType;
@@ -31,6 +33,82 @@ abstract class GLODataType {
   public abstract print(): string;
 
   public static promotable?: typeof GLODataType[];
+
+  add(right: GLODataType): GLODataType {
+    throw new Error(
+      `Program error: Cannot perform add operation with type ${this.constructor.name}`,
+    );
+  }
+  subtract(right: GLODataType): GLODataType {
+    throw new Error(
+      `Program error: Cannot perform subtract operation with type ${this.constructor.name}`,
+    );
+  }
+  multiply(right: GLODataType): GLODataType {
+    throw new Error(
+      `Program error: Cannot perform multiply operation with type ${this.constructor.name}`,
+    );
+  }
+  divide(right: GLODataType): GLODataType {
+    throw new Error(
+      `Program error: Cannot perform divide operation with type ${this.constructor.name}`,
+    );
+  }
+  integerDivide(right: GLODataType): GLODataType {
+    throw new Error(
+      `Program error: Cannot perform integerDivide operation with type ${this.constructor.name}`,
+    );
+  }
+  exponent(right: GLODataType): GLODataType {
+    throw new Error(
+      `Program error: Cannot perform exponent operation with type ${this.constructor.name}`,
+    );
+  }
+  mod(right: GLODataType): GLODataType {
+    throw new Error(
+      `Program error: Cannot perform mod operation with type ${this.constructor.name}`,
+    );
+  }
+  equals(right: GLODataType): boolean {
+    throw new Error(
+      `Program error: Cannot perform equals operation with type ${this.constructor.name}`,
+    );
+  }
+  notEquals(right: GLODataType): boolean {
+    throw new Error(
+      `Program error: Cannot perform notEquals operation with type ${this.constructor.name}`,
+    );
+  }
+  lessThan(right: GLODataType): boolean {
+    throw new Error(
+      `Program error: Cannot perform lessThan operation with type ${this.constructor.name}`,
+    );
+  }
+  greaterThan(right: GLODataType): boolean {
+    throw new Error(
+      `Program error: Cannot perform greaterThan operation with type ${this.constructor.name}`,
+    );
+  }
+  lessEqualsThan(right: GLODataType): boolean {
+    throw new Error(
+      `Program error: Cannot perform lessEqualsThan operation with type ${this.constructor.name}`,
+    );
+  }
+  greaterEqualsThan(right: GLODataType): boolean {
+    throw new Error(
+      `Program error: Cannot perform greaterEqualsThan operation with type ${this.constructor.name}`,
+    );
+  }
+  unaryPlus(): GLODataType {
+    throw new Error(
+      `Program error: Cannot perform unaryPlus operation with type ${this.constructor.name}`,
+    );
+  }
+  unaryMinus(): GLODataType {
+    throw new Error(
+      `Program error: Cannot perform unaryMinus operation with type ${this.constructor.name}`,
+    );
+  }
 }
 
 export default GLODataType;
